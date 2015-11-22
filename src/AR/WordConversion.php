85a220259dc5b3563d85c2ccb4859f206e7d1610
Amarjeet Singh Rai <jatt27@gmail.com>
2015-11-22 12:50:07 +0000

    Make zero to nine conversion tests pass

--- src/AR/WordConversion.php
+++ src/AR/WordConversion.php
@@ -5,6 +5,43 @@ class WordConversion
 {
     public function convert($number)
     {
+        $output = '';
 
+        if (in_array($number, range(0, 9))) {
+            switch ($number) {
+                case 0:
+                    $output = 'zero';
+                    break;
+                case 1:
+                    $output = 'one';
+                    break;
+                case 2:
+                    $output = 'two';
+                    break;
+                case 3:
+                    $output = 'three';
+                    break;
+                case 4:
+                    $output = 'four';
+                    break;
+                case 5:
+                    $output = 'five';
+                    break;
+                case 6:
+                    $output = 'six';
+                    break;
+                case 7:
+                    $output = 'seven';
+                    break;
+                case 8:
+                    $output = 'eight';
+                    break;
+                case 9:
+                    $output = 'nine';
+                    break;
+            }
+        }
+
+        return $output;
     }
 }
