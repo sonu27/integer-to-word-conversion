09c073d0b46ccd6d7ff9fdd632833cb921aae305
Amarjeet Singh Rai <jatt27@gmail.com>
2015-11-22 14:48:33 +0000

    Add 10 to 19, and tens conversion tests and make them pass

--- src/AR/WordConversion.php
+++ src/AR/WordConversion.php
@@ -3,43 +3,123 @@ namespace AR;
 
 class WordConversion
 {
+    private static $tens = [
+        1 => 'ten',
+        2 => 'twenty',
+        3 => 'thirty',
+        4 => 'forty',
+        5 => 'fifty',
+        6 => 'sixty',
+        7 => 'seventy',
+        8 => 'eighty',
+        9 => 'ninety',
+    ];
+
     public function convert($number)
     {
         $output = '';
 
-        if (in_array($number, range(0, 9))) {
-            switch ($number) {
-                case 0:
-                    $output = 'zero';
-                    break;
-                case 1:
-                    $output = 'one';
-                    break;
-                case 2:
-                    $output = 'two';
-                    break;
-                case 3:
-                    $output = 'three';
-                    break;
-                case 4:
-                    $output = 'four';
-                    break;
-                case 5:
-                    $output = 'five';
-                    break;
-                case 6:
-                    $output = 'six';
-                    break;
-                case 7:
-                    $output = 'seven';
-                    break;
-                case 8:
-                    $output = 'eight';
-                    break;
-                case 9:
-                    $output = 'nine';
-                    break;
+        if ($number === 0) {
+            return 'zero';
+        }
+
+        $numberString = str_pad(strval($number), 9, '0', STR_PAD_LEFT);
+        $subString = substr($numberString, -3);
+
+        $output = $this->convertLessThan1000($number);
+
+        return $output;
+    }
+
+    private function convertLessThan1000($number)
+    {
+        $output = '';
+        if ($number < 20) {
+            $output = $this->convert0To19($number);
+        } else {
+            $numberString = str_pad(strval($number), 3, '0', STR_PAD_LEFT);
+
+            if ($numberString[0] !== '0') {
+                $output = $this->convert0To19(intval($numberString[0])).' hundred';
             }
+
+            if ($numberString[0] !== '0' && ($numberString[1] !== '0' || $numberString[2] !== '0')) {
+                $output .= ' and ';
+            }
+
+            if ($numberString[1] !== '0') {
+                $output .= self::$tens[intval($numberString[1])];
+            }
+
+            if ($numberString[2] !== '0') {
+                $output .= $this->convert0To19(intval($numberString[2]));
+            }
+        }
+
+        return $output;
+    }
+
+    private function convert0To19($number)
+    {
+        $output = '';
+        switch ($number) {
+            case 1:
+                $output = 'one';
+                break;
+            case 2:
+                $output = 'two';
+                break;
+            case 3:
+                $output = 'three';
+                break;
+            case 4:
+                $output = 'four';
+                break;
+            case 5:
+                $output = 'five';
+                break;
+            case 6:
+                $output = 'six';
+                break;
+            case 7:
+                $output = 'seven';
+                break;
+            case 8:
+                $output = 'eight';
+                break;
+            case 9:
+                $output = 'nine';
+                break;
+            case 10:
+                $output = 'ten';
+                break;
+            case 11:
+                $output = 'eleven';
+                break;
+            case 12:
+                $output = 'twelve';
+                break;
+            case 13:
+                $output = 'thirteen';
+                break;
+            case 14:
+                $output = 'fourteen';
+                break;
+            case 15:
+                $output = 'fifteen';
+                break;
+            case 16:
+                $output = 'sixteen';
+                break;
+            case 17:
+                $output = 'seventeen';
+                break;
+            case 18:
+                $output = 'eighteen';
+                break;
+            case 19:
+                $output = 'nineteen';
+                break;
         }
 
         return $output;
--- tests/AR/WordConversionTest.php
+++ tests/AR/WordConversionTest.php
@@ -58,4 +58,94 @@ class WordConversionTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('nine', $this->obj->convert(9));
     }
+
+    public function test10Conversion()
+    {
+        $this->assertEquals('ten', $this->obj->convert(10));
+    }
+
+    public function test11Conversion()
+    {
+        $this->assertEquals('eleven', $this->obj->convert(11));
+    }
+
+    public function test12Conversion()
+    {
+        $this->assertEquals('twelve', $this->obj->convert(12));
+    }
+
+    public function test13Conversion()
+    {
+        $this->assertEquals('thirteen', $this->obj->convert(13));
+    }
+
+    public function test14Conversion()
+    {
+        $this->assertEquals('fourteen', $this->obj->convert(14));
+    }
+
+    public function test15Conversion()
+    {
+        $this->assertEquals('fifteen', $this->obj->convert(15));
+    }
+
+    public function test16Conversion()
+    {
+        $this->assertEquals('sixteen', $this->obj->convert(16));
+    }
+
+    public function test17Conversion()
+    {
+        $this->assertEquals('seventeen', $this->obj->convert(17));
+    }
+
+    public function test18Conversion()
+    {
+        $this->assertEquals('eighteen', $this->obj->convert(18));
+    }
+
+    public function test19Conversion()
+    {
+        $this->assertEquals('nineteen', $this->obj->convert(19));
+    }
+
+    public function test20Conversion()
+    {
+        $this->assertEquals('twenty', $this->obj->convert(20));
+    }
+
+    public function test30Conversion()
+    {
+        $this->assertEquals('thirty', $this->obj->convert(30));
+    }
+
+    public function test40Conversion()
+    {
+        $this->assertEquals('forty', $this->obj->convert(40));
+    }
+
+    public function test50Conversion()
+    {
+        $this->assertEquals('fifty', $this->obj->convert(50));
+    }
+
+    public function test60Conversion()
+    {
+        $this->assertEquals('sixty', $this->obj->convert(60));
+    }
+
+    public function test70Conversion()
+    {
+        $this->assertEquals('seventy', $this->obj->convert(70));
+    }
+
+    public function test80Conversion()
+    {
+        $this->assertEquals('eighty', $this->obj->convert(80));
+    }
+
+    public function test90Conversion()
+    {
+        $this->assertEquals('ninety', $this->obj->convert(90));
+    }
 }
